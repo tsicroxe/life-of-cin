6f413802bd7f0cae498faf981ca0dbf4e10e2e85
tsicroxE <arbanakus@gmail.com>
2020-05-30 16:11:20 -0700

    cheeky text change

--- resources/views/home/recent_countries.blade.php
+++ resources/views/home/recent_countries.blade.php
@@ -21,7 +21,7 @@
         <img src="https://lh6.googleusercontent.com/-8YJEFFtPA-M/AAAAAAAAAAI/AAAAAAAAADA/aLobp4soAZQ/photo.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Pretty Mama</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text">Tropical drinks melting in your hand.</p>
             <a href="#" class="btn btn-primary">Learn more</a>
         </div>
     </div>
