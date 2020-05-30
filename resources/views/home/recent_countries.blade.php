8257608606b0010059bea04e322f44358cab5c63
tsicroxE <arbanakus@gmail.com>
2020-05-30 16:09:56 -0700

    cheeky text change

--- resources/views/home/recent_countries.blade.php
+++ resources/views/home/recent_countries.blade.php
@@ -3,8 +3,8 @@
         <img src="https://www.gotobermuda.com/sites/default/files/styles/share/public/bermuda-island-aerial.jpg?itok=Oy0zbven" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Bermuda</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <p class="card-text">That's where I want to get away from it all.</p>
+            <a href="#" class="btn btn-primary">Read more!</a>
         </div>
     </div>
 
@@ -12,8 +12,8 @@
         <img src="https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_891,q_50,w_1402/v1/clients/jamaica/HEARTBEAT_dophin_cove_5babe1eb-18c2-4705-8caf-dd31c68e5e8f.png" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Bahamas</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <p class="card-text">Off the florida keys, there's a place called Kokomo</p>
+            <a href="#" class="btn btn-primary">Koko..GO</a>
         </div>
     </div>
 
@@ -22,7 +22,7 @@
         <div class="card-body">
             <h5 class="card-title">Pretty Mama</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary">Learn more</a>
         </div>
     </div>
 </div>
